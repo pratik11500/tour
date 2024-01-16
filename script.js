2575bc28d6617c3dea25f02612bd1e7064c7934a
Pratik <139073467+pratik11500@users.noreply.github.com>
2024-01-16 12:45:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+$(window).on('scroll', function() {
+  if ($(window).scrollTop()) {
+    $('header').addClass('nav-show');
+  } else {
+    $('header').removeClass('nav-show');
+  }
+})
+const navSlide = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".nav-bar");
+  const navLinks = document.querySelectorAll(".nav-bar li");
+  hamburger.onclick = () => {
+    navbar.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+      }
+    }
+    );
+    hamburger.classList.toggle("toggle");
+  }
+}
+window.onload = () => navSlide();
+const updateVisitorCount = async () => {
+  try {
+    const response = await fetch('#');
+    const result = await response.json();
+    document.getElementById('visitor-count').innerHTML = result.value;
+  } catch (error) {
+    console.error(error);
+  }
+}
+  ;
+updateVisitorCount();
+
+
+
+
+
+var blockContextMenu = function (e) {
+    // Prevent right-click context menu
+    e.preventDefault();
+};
+
+var showWarning = function () {
+    var warningPopup = document.createElement('div');
+    warningPopup.innerHTML = '<p>Content is Protected!</p>';
+    warningPopup.classList.add('warning-popup');
+    document.body.appendChild(warningPopup);
+
+    // Remove the warning after a delay (e.g., 2 seconds)
+    setTimeout(function () {
+        document.body.removeChild(warningPopup);
+    }, 2000);
+};
+
+document.addEventListener('contextmenu', blockContextMenu);
+document.addEventListener('keydown', function (e) {
+    // Disable F12 key
+    if (e.key === 'F12' || e.keyCode === 123) {
+        e.preventDefault();
+        showWarning();
+    }
+});
